b5ca454e14270243273a38d77b05a6888986bf05
jinonensk <jinonensk@gmail.com>
2019-06-09 14:10:44 +0700

    fixed after big review. + work on the index page

--- js/javascript.js
+++ js/javascript.js
@@ -29,3 +29,76 @@ if (document.querySelector('.slider')) {
     })
   });
 };
+
+// popup
+if (document.querySelector('.map-contacts .button')) {
+  const invokePopup = document.querySelector('.map-contacts .button');
+  const askPopup = document.querySelector('.ask-popup');
+  const darkOverlay = document.querySelector('.dark-overlay')
+
+  const askClose = askPopup.querySelector('.button-close');
+  const askForm = askPopup.querySelector('form');
+
+  const userName = askPopup.querySelector('[name=user-name]');
+  const userEmail = askPopup.querySelector('[name=popup-user-email]');
+  const askMessage = askPopup.querySelector('[name=user-text]');
+
+  let isStorageSupport = true;
+  let storage = '';
+
+  try {
+    storage = localStorage.getItem('name');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  invokePopup.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    askPopup.classList.add('popup-show');
+    darkOverlay.classList.add('overlay-show');
+    userName.focus();
+
+    if (storage) {
+      userName.value = storage;
+      userEmail.value = localStorage.getItem('email')
+      askMessage.focus();
+    } else {
+      userName.focus();
+    };
+  });
+
+  askClose.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    askPopup.classList.remove('popup-show');
+    darkOverlay.classList.remove('overlay-show');
+    askPopup.classList.remove('popup-ask-error');
+  });
+
+  askForm.addEventListener('submit', function(evt) {
+
+    if (!userName.value || !userEmail.value || !askMessage.value) {
+      evt.preventDefault();
+      askPopup.classList.remove('popup-ask-error');
+      askPopup.offsetWidth = askPopup.offsetWidth;
+      askPopup.classList.add('popup-ask-error');
+    } else {
+
+      if (isStorageSupport) {
+        localStorage.setItem('name', userName.value);
+        localStorage.setItem('email', userEmail.value);
+      };
+    };
+  });
+
+  window.addEventListener('keydown', function(evt) {
+    if (askPopup.classList.contains('popup-show')) {
+
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        askPopup.classList.remove('popup-show');
+        darkOverlay.classList.remove('overlay-show');
+        askPopup.classList.remove('popup-ask-error');
+      };
+    };
+  });
+};
